d536ae38c978010379909f486ee42a88021d0fd4
e4jet <e4jet@1aa0c6fe-c44d-0410-af6a-ad80a4552a29>
2009-08-13 03:40:28 +0000

    Added  waitForTask() to Task.java. This is a "drop-in" replacement for waitForMe() that uses a timed polling in place of waitForValues.
    -Eric Forgette (forgette@netapp.com)

--- src/com/vmware/vim25/mo/Task.java
+++ src/com/vmware/vim25/mo/Task.java
@@ -4,14 +4,14 @@ Copyright (c) 2008 VMware, Inc. All Rights Reserved.
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, 
+ * Redistributions of source code must retain the above copyright notice, 
 this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice, 
+ * Redistributions in binary form must reproduce the above copyright notice, 
 this list of conditions and the following disclaimer in the documentation 
 and/or other materials provided with the distribution.
 
-* Neither the name of VMware, Inc. nor the names of its contributors may be used
+ * Neither the name of VMware, Inc. nor the names of its contributors may be used
 to endorse or promote products derived from this software without specific prior 
 written permission.
 
@@ -48,67 +48,201 @@ import com.vmware.vim25.TaskInfoState;
 
 public class Task extends ExtensibleManagedObject 
 {
-	public static final String PROPNAME_INFO = "info";
-	public static final String SUCCESS = "success";
+    public static final String PROPNAME_INFO = "info";
+    public static final String SUCCESS = "success";
 
-	public Task(ServerConnection serverConnection, ManagedObjectReference mor) 
-	{
-		super(serverConnection, mor);
-	}
+    public Task(ServerConnection serverConnection, ManagedObjectReference mor) 
+    {
+        super(serverConnection, mor);
+    }
 
-	public TaskInfo getTaskInfo() throws InvalidProperty, RuntimeFault, RemoteException 
-	{
-		return (TaskInfo) getCurrentProperty(PROPNAME_INFO);
-	}
-	
-	public ManagedEntity getAssociatedManagedEntity()
-	{
-		return (ManagedEntity) getManagedObject("info.entity");
-	}
-	
-	public ManagedEntity[] getLockedManagedEntities()
-	{
-		return (ManagedEntity[]) getManagedObjects("info.locked");
-	}
-	
-	public void cancelTask() throws RuntimeFault, RemoteException 
-	{
-		getVimService().cancelTask(getMOR());
-	}
-	
-	public void setTaskState(TaskInfoState tis, Object result, LocalizedMethodFault fault) throws InvalidState, RuntimeFault, RemoteException 
-	{
-		getVimService().setTaskState(getMOR(), tis, result, fault);
-	}
-	
-	public void updateProgress(int percentDone) throws InvalidState, OutOfBounds, RuntimeFault, RemoteException 
-	{
-		getVimService().updateProgress(getMOR(), percentDone);
-	}
-	
-	public String waitForMe() throws InvalidProperty, RuntimeFault, MethodFault, RemoteException  
-	{   	  
-		Object[] result = waitForValues(
-		                        new String[] { "info.state", "info.error" }, 
-		                        new String[] { "state" },
-		                        new Object[][] { new Object[] { TaskInfoState.success, TaskInfoState.error } });      
+    public TaskInfo getTaskInfo() throws InvalidProperty, RuntimeFault, RemoteException 
+    {
+        return (TaskInfo) getCurrentProperty(PROPNAME_INFO);
+    }
 
-		if (result[0].equals(TaskInfoState.success)) 
-		{      	 
-			return SUCCESS;
-		}
-		else 
-		{
-			TaskInfo tinfo = (TaskInfo) getCurrentProperty(PROPNAME_INFO);      	 
-			LocalizedMethodFault fault = tinfo.getError();
-			String error = "Error Occured";
-			if(fault!=null) 
-			{
-				MethodFault mf = fault.getFault();
-				throw mf;
-			}
-			return error;
-		}
-	}
-	
+    public ManagedEntity getAssociatedManagedEntity()
+    {
+        return (ManagedEntity) getManagedObject("info.entity");
+    }
+
+    public ManagedEntity[] getLockedManagedEntities()
+    {
+        return (ManagedEntity[]) getManagedObjects("info.locked");
+    }
+
+    public void cancelTask() throws RuntimeFault, RemoteException 
+    {
+        getVimService().cancelTask(getMOR());
+    }
+
+    public void setTaskState(TaskInfoState tis, Object result, LocalizedMethodFault fault) throws InvalidState, RuntimeFault, RemoteException 
+    {
+        getVimService().setTaskState(getMOR(), tis, result, fault);
+    }
+
+    public void updateProgress(int percentDone) throws InvalidState, OutOfBounds, RuntimeFault, RemoteException 
+    {
+        getVimService().updateProgress(getMOR(), percentDone);
+    }
+
+    /**
+     * If there is another thread or client calling waitForUpdate(), the behavior of this
+     * method is not predictable. This usually happens with VI Client plug-in which shares
+     * the session with the VI Client which use waitForUpdate() extensively.
+     * The safer way is to poll the related info.state and check its value.
+     * @return
+     * @throws InvalidProperty
+     * @throws RuntimeFault
+     * @throws RemoteException
+     * @deprecated
+     */
+    public String waitForMe() throws InvalidProperty, RuntimeFault, RemoteException  
+    {   
+
+        Object[] result = waitForValues(
+                new String[] { "info.state", "info.error" }, 
+                new String[] { "state" },
+                new Object[][] { new Object[] { TaskInfoState.success, TaskInfoState.error } });      
+
+        if (result[0].equals(TaskInfoState.success)) 
+        {      	 
+            return SUCCESS;
+        }
+        else 
+        {
+            TaskInfo tinfo = (TaskInfo) getCurrentProperty(PROPNAME_INFO);      	 
+            LocalizedMethodFault fault = tinfo.getError();
+            String error = "Error Occured";
+            if(fault!=null) 
+            {
+                MethodFault mf = fault.getFault();
+                throw mf;
+            }
+            return error;
+        }
+    }
+
+    /**
+     * Copyright 2009 NetApp, contribution by Eric Forgette
+     * 
+     * @author Eric Forgette (forgette@netapp.com)
+     * 
+     * This is a "drop-in" replacement for waitForMe() that uses a timed polling
+     * in place of waitForValues.
+     * 
+     * This method will eat 3 exceptions while trying to get TaskInfo and TaskState.
+     * On the fourth try, the exception is captured and thrown.
+     * 
+     * @return String based on TaskInfoState, or "Error Occured"
+     * @throws InvalidProperty
+     * @throws RuntimeFault
+     * @throws RemoteException
+     * @throws InterruptedException 
+     */
+    public String waitForTask() throws InvalidProperty, RuntimeFault, RemoteException, InterruptedException  
+    {         
+        TaskInfo tInfo = null;
+        TaskInfoState tState = null;
+        int tries = 0;
+        int maxTries=3;
+        Exception getInfoException = null;
+
+        while (tInfo == null || tState == null)
+        {
+            try
+            {
+                tInfo = getTaskInfo();
+                tState = tInfo.getState();
+            }
+            catch (InvalidProperty e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                //silently catch 3 exceptions (other than InvalidProperty)
+                getInfoException = e;
+            }
+
+            if (tInfo == null || tState == null)
+            {
+                // tInfo or tState was null, so we sleep 1 second
+                Thread.sleep(1000);
+                tries++;
+                if (tries > maxTries)
+                {
+                    if (getInfoException == null)
+                    {
+                        throw new NullPointerException();
+                    }
+                    else if (getInfoException instanceof RuntimeFault)
+                    {
+                        throw (RuntimeFault) getInfoException;
+                    }
+                    else if (getInfoException  instanceof RemoteException )
+                    {
+                        throw (RemoteException) getInfoException;
+                    }
+                }
+            }
+        }
+
+        if (tInfo != null){
+            while(tState.equals(TaskInfoState.running) || tState.equals(TaskInfoState.queued)){
+                //if running wait 0.5 seconds, if queued wait 1 second
+                if (tState.equals(TaskInfoState.running))
+                {
+                    Thread.sleep(500);
+                }
+                else
+                {
+                    Thread.sleep(1000);
+                }
+
+                tState = null;
+                getInfoException = null;
+                tries=0;
+                while (tState==null)
+                {
+                    try
+                    {
+                        tState = getTaskInfo().getState();
+                    }
+                    catch (InvalidProperty e)
+                    {
+                        throw e;
+                    }
+                    catch (Exception e)
+                    {
+                        getInfoException=e;
+                    }
+                    if (tState == null)
+                    {
+                        Thread.sleep(1000);
+                        tries++;
+                        if (tries > maxTries)
+                        {
+                            if (getInfoException == null)
+                            {
+                                throw new NullPointerException();
+                            }
+                            else if (getInfoException instanceof RuntimeFault)
+                            {
+                                throw (RuntimeFault) getInfoException;
+                            }
+                            else if (getInfoException  instanceof RemoteException )
+                            {
+                                throw (RemoteException) getInfoException;
+                            }
+                        }
+                    }
+                }
+
+            }
+            return tState.toString();
+        }
+        return "Error Occured";
+    }
+
 }
